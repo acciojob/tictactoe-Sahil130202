d0f9e519c214dba420736f5184bff096fee83f8d
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-07 12:17:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,78 +1,85 @@
-//your JS code here. If required.
-let player1 = document.getElementById("player-1")
-let player1 = document.getElementById("player-2")
-let submit = document.getElementById("submit")
+let player1 = document.getElementById("player-1");
+let player2 = document.getElementById("player-2");
+let submit = document.getElementById("submit");
 
-let game = document.getElementById("game")
-let message = document.getElementById(".message")
+let game = document.getElementById("game");
+let message = document.querySelector(".message");
 
-let currentPlayer = 1
-let board = ["", "", "", "", "", "", "", "", ""]
-let winningCombinations = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
-];
+let currentPlayer = "x";
 
-submit.addEventListener("click", function(){
-	let name1 = player1.value
-	let name2 = player2.value
-	game.style.display="block"
-	player1.parentElement.style.display = "none"
+submit.addEventListener("click", function () {
+    let name1 = player1.value;
+    let name2 = player2.value;
 
-	message.innerText = name1 + ", you're up"
-	let cells = document.querySelectorAll(".cell")
+    game.style.display = "block";
+    player1.parentElement.style.display = "none";
 
-	cells.forEach(function (cell){
-		cell.addEventListener("click", function(){
-			let id = Number(cell.id)
-			if(board.id!==""){
-				return
-			}
-			 if (currentPlayer === 1) {
-                cell.innerText = "x";
-                board[id] = "x";
-            } else {
-                cell.innerText = "o";
-                board[id] = "o";
-			 }
-			for(let combinations in winningCombinations){
-				 let a = board[combination[0]];
-                let b = board[combination[1]];
-                let c = board[combination[2]];
-				if(a!=="" && a===b && b===c){
-					if (currentPlayer === 1) {
-                        message.innerText =
-                            name1 + " congratulations you won!";
-                    } else {
-                        message.innerText =
-                            name2 + " congratulations you won!";
-                    }
+    message.innerText = name1 + ", you're up";
 
-                    return;
-				}
-			}
-			if (currentPlayer === 1) {
-                currentPlayer = 2;
+    let cells = document.querySelectorAll(".cell");
+
+    cells.forEach(function (cell) {
+        cell.addEventListener("click", function () {
+
+            // Don't overwrite an existing move
+            if (cell.innerText !== "") {
+                return;
+            }
+
+            cell.innerText = currentPlayer;
+
+            // Check winner
+            let win = checkWinner();
+
+            if (win) {
+                if (currentPlayer === "x") {
+                    message.innerText =
+                        name1 + " congratulations you won!";
+                } else {
+                    message.innerText =
+                        name2 + " congratulations you won!";
+                }
+                return;
+            }
+
+            // Change player
+            if (currentPlayer === "x") {
+                currentPlayer = "o";
                 message.innerText = name2 + ", you're up";
             } else {
-                currentPlayer = 1;
+                currentPlayer = "x";
                 message.innerText = name1 + ", you're up";
             }
-		})
-	})
-	
-
-	
-	
-} )
+        });
+    });
+});
 
 
+function checkWinner() {
 
+    let winningCombinations = [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"],
 
+        ["1", "4", "7"],
+        ["2", "5", "8"],
+        ["3", "6", "9"],
 
+        ["1", "5", "9"],
+        ["3", "5", "7"]
+    ];
+
+    for (let combination of winningCombinations) {
+
+        let a = document.getElementById(combination[0]).innerText;
+        let b = document.getElementById(combination[1]).innerText;
+        let c = document.getElementById(combination[2]).innerText;
+
+        if (a !== "" && a === b && b === c) {
+            return true;
+        }
+    }
+
+    return false;
+}
